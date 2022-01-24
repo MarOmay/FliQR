6a0b33626cf34ac1c01d08d2628f7da5fcb14317
MarOmay <47245545+MarOmay@users.noreply.github.com>
2022-01-24 17:34:34 +0800

    Added Generate QR and Save result as image functionality

--- app/src/main/java/com/fliqr/dev/Output.java
+++ app/src/main/java/com/fliqr/dev/Output.java
@@ -1,23 +1,116 @@
 package com.fliqr.dev;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Output extends AppCompatActivity {
+
+    private Bitmap bitmap;
+
+    private ImageView result;
+    private Button save, close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output);
+
+        byte[] byteArray = getIntent().getByteArrayExtra("BitmapQR");
+        bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+
+        result = findViewById(R.id.outputQr);
+        result.setImageBitmap(bitmap);
+
+        save = findViewById(R.id.save_button);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Generate date and time for unique file name
+                String filename = "";
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy-HHmm.ss");
+                Date date = new Date();
+                filename = formatter.format(date) + ".jpeg";
+
+                try {
+                    File storageDir = new File(Environment
+                            .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/FliQR/");
+                    if (!storageDir.exists())
+                        storageDir.mkdirs();
+
+                    ContextWrapper cw = new ContextWrapper(getApplicationContext());
+                    File file = new File(storageDir, filename);
+
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fos);
+                        Toast.makeText(getApplicationContext(), "Image saved", Toast.LENGTH_SHORT).show();
+                        fos.flush();
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), "Saving image unsuccessful", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            fos.close();
+                        } catch (IOException e) {
+                            Toast.makeText(getApplicationContext(), "Saving image unsuccessful", Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                        }
+                    }
+
+                    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                    Uri contentUri = Uri.fromFile(file);
+                    mediaScanIntent.setData(contentUri);
+                    cw.sendBroadcast(mediaScanIntent);
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Saving image unsuccessful", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        close = findViewById(R.id.close_button);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Output.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                Output.this.startActivity(intent);
+            }
+        });
     }
 
     public void gotoMainActivity(View view){
         Intent intent = new Intent(Output.this, MainActivity.class);
         Output.this.startActivity(intent);
         Output.this.finish();
+    }
+
+    public void back(View view){
+        onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
     }
 
 
--- app/src/main/java/com/fliqr/dev/Text.java
+++ app/src/main/java/com/fliqr/dev/Text.java
@@ -3,25 +3,85 @@ package com.fliqr.dev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class Text extends AppCompatActivity {
+
+    private TextView text;
+    private Button generate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
+
+        text = findViewById(R.id.editText);
+        generate =  findViewById(R.id.generate_button);
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (text.getText().toString().length() < 1){
+                    Toast.makeText(getApplicationContext(), "Please input text", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+                    Display display = windowManager.getDefaultDisplay();
+                    Point point = new Point();
+                    display.getSize(point);
+                    int width = point.x;
+                    int height = point.y;
+                    int dimen = width < height ? width : height;
+                    QRGEncoder qrgEncoder = new QRGEncoder(text.getText().toString(),null, QRGContents.Type.TEXT, dimen);
+
+                    try {
+                        Bitmap bitmap = qrgEncoder.encodeAsBitmap();
+
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                        byte[] byteArray = stream.toByteArray();
+
+                        Intent intent =  new Intent(Text.this, Output.class);
+                        intent.putExtra("BitmapQR", byteArray);
+
+                        Text.this.startActivity(intent);
+                    }
+                    catch (Exception e){
+                        Toast.makeText(getApplicationContext(), "Can't generate QR", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
-    public void back(View view){
-        finish();
-    }
 
     public void gotoOutput(View view){
         Intent intent = new Intent(Text.this, Output.class);
         Text.this.startActivity(intent);
         Text.this.finish();
+    }
+
+    public void back(View view){
+        onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
     }
 
 }
