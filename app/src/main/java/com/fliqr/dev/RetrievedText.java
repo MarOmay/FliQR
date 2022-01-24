1542fb582d307c10995fdda5217c48a3a35461b7
charleology <cnbelano@gmail.com>
2022-01-24 20:49:12 +0800

    Added layout UIs

---
+++ app/src/main/java/com/fliqr/dev/RetrievedText.java
@@ -0,0 +1,14 @@
+package com.fliqr.dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RetrievedText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieved_text);
+    }
+}
