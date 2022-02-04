ccea5a029ac5cf0c1cd767831d49d4d7811d8cd8
MarOmay <47245545+MarOmay@users.noreply.github.com>
2022-02-04 14:06:16 +0800

    Clean Up (ImportQr): removed unused imports

--- app/src/main/java/com/fliqr/dev/ImportQr.java
+++ app/src/main/java/com/fliqr/dev/ImportQr.java
@@ -3,7 +3,6 @@ package com.fliqr.dev;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
